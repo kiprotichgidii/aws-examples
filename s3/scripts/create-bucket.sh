7117fd03c860548da0ad57e081131a32c9d06063
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-01-31 12:39:40 +0300

    Update create-bucket script

--- s3/scripts/create-bucket.sh
+++ s3/scripts/create-bucket.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+echo "Creating bucket: $1"
 
+# Check if the bucket name is provided
 if [ -z "$1" ]; then
     echo "You must specify a bucket name: e.g, ./create-bucket my-example-bucket"
     exit 1
@@ -8,6 +10,17 @@ fi
 BUCKET_NAME=$1
 REGION=${2:-"us-east-1"}
 
-aws s3api create-bucket \
-  --bucket $BUCKET_NAME \
-  --create-bucket-configuration="LocationConstraint=$REGION"
+if [ "$REGION" == "us-east-1" ]; then
+  aws s3api create-bucket \
+    --bucket $BUCKET_NAME \
+    --region $REGION \
+    --query 'Location' \
+    --output text
+else
+  aws s3api create-bucket \
+    --bucket $BUCKET_NAME \
+    --region $REGION \
+    --create-bucket-configuration="LocationConstraint=$REGION" \
+    --query 'Location' \
+    --output text
+fi
