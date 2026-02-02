2745a7580b94e92c609a2a99962ba69f02089c81
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-02-02 12:24:55 +0300

    Add etags main.tf

---
+++ s3/etags/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+}
+
+resource "aws_s3_bucket" "amzn-bucket" {
+  #bucket = "amzn-tf-s3-bucket"
+
+  tags = {
+    Name        = "My Terraform bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.amzn-bucket.id
+  key    = "index.html"
+  source = "index.html"
+
+  etag = filemd5("index.html")
+}
